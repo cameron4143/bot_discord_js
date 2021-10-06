cc1f115bff0044fedbf35cc7dc39009857e3bcf0
cameron4143 <cameron35.CR@gmail.com>
2021-10-06 11:03:07 +0200

    la chance

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login("ODkyNjkyOTM4NDAyOTU1Mjk1.YVQnFQ.QD1AjCuqWz8uxFYa-zAPvMUYtvQ");
+
+
